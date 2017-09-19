7acc240c34f16b055d94e83b9de9860e15dea66f
Simon Lydell <simon.lydell@gmail.com>
2017-09-19 13:38:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# babel-plugin-minify-dead-code-elimination bug demo
+
+1. `npm install`
+2. `npm run build`
+3. Open `pass.html` in a browser.
+4. Open `fail.html` in a browser.
+
+Tip: To see the diff between input and output:
+
+```
+git diff --no-index jquery.input.js jquery.output.js
+```
+
+## Expected behavior (before minify)
+
+![pass.gif](pass.gif)
+
+## Actual behavior (after minify)
+
+![fail.gif](fail.gif)
---
+++ minify.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const fs = require("fs");
+const babel = require("babel-core");
+
+const input = fs.readFileSync("node_modules/jquery/dist/jquery.js", "utf8");
+
+const result1 = babel.transform(input);
+
+const result2 = babel.transform(result1.code, {
+  plugins: ["minify-dead-code-elimination"],
+});
+
+assert(result2.code.length < result1.code.length);
+
+fs.writeFileSync("jquery.original.js", input);
+fs.writeFileSync("jquery.input.js", result1.code);
+fs.writeFileSync("jquery.output.js", result2.code);
